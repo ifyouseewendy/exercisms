5bad15c07d24285de5db4f62a8983d4d769a6643
Di Wen <ifyouseewendy@gmail.com>
2018-03-06 16:12:52 -0500

    Opt grains* by using <-*

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -5,7 +5,7 @@ import Data.Maybe (fromJust)
 square :: Integer -> Maybe Integer
 square n
   | n <= 0 || n > 64 = Nothing
-  | otherwise = Just (2^(n-1))
+  | otherwise = Just $ 2 ^ (n-1)
 
 total :: Integer
-total = sum $ map (fromJust . square) [1..64]
+total = sum [(fromJust . square) n | n <- [1..64]]
