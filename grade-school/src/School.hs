042e4be29e6e9ec69807ae1c5079d14e0f0bf68e
Di Wen <ifyouseewendy@gmail.com>
2018-03-28 13:59:00 -0400

    Opt grade-schools with findWithDefault

--- grade-school/src/School.hs
+++ grade-school/src/School.hs
@@ -2,7 +2,6 @@ module School (School, add, empty, grade, sorted) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Maybe (fromJust, isNothing)
 import Data.List (sort, sortOn)
 
 type School = Map Int [String]
@@ -16,12 +15,13 @@ empty :: School
 empty = Map.empty
 
 grade :: Int -> School -> [String]
-grade gradeNum school
-  | isNothing v = []
-  | otherwise = sort (fromJust v)
-    where v = (Map.!?) school gradeNum
+grade gradeNum school =
+  sort $ Map.findWithDefault [] gradeNum school
 
 sorted :: School -> [(Int, [String])]
 sorted school =
-  map (\t -> (fst t, sort (snd t))) (sortOn fst list)
-  where list = Map.toList school
+  map sortSnd sortedWithGrade
+  where
+    sortSnd (first, second) = (first, sort second)
+    sortedWithGrade = sortOn fst list
+    list = Map.toList school
