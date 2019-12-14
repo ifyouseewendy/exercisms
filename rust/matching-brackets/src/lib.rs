0c300f30d478197826564d9026b0afad38d38a8b
Di Wen <ifyouseewendy@gmail.com>
2019-12-14 16:00:07 -0500

    rs: Refactor matching-brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,32 +1,14 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut s = string.to_string();
-    s.retain(is_target);
-
     let mut stack = vec![];
 
-    for c in s.chars() {
-        if stack.is_empty() {
-            stack.push(c);
-        } else {
-            match stack.last() {
-                Some('[') => {
-                    if c == ']' { stack.pop(); } else { stack.push(c); }
-                },
-                Some('(') => {
-                    if c == ')' { stack.pop(); } else { stack.push(c); }
-                },
-                Some('{') => {
-                    if c == '}' { stack.pop(); } else { stack.push(c); }
-                },
-                Some(_) => (),
-                None => (),
-            }
+    for c in string.chars() {
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' => if stack.pop() != Some('(') { return false },
+            ']' => if stack.pop() != Some('[') { return false },
+            '}' => if stack.pop() != Some('{') { return false },
+            _ => ()
         }
     }
-
     stack.is_empty()
-}
-
-fn is_target(c: char) -> bool {
-    c == '[' || c == ']' || c == '(' || c == ')' || c == '{' || c == '}'
 }
