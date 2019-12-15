2fa1f3c4c089a24b5c89244afe2dca43bad714c1
Di Wen <ifyouseewendy@gmail.com>
2019-12-14 21:19:28 -0500

    rs: Refactor with char::to_digit

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     if num == 0 { return true; }
 
-    let str_num = num.to_string();
-    let len = str_num.len() as u32;
+    let num_str = num.to_string();
+    let len = num_str.len() as u32;
 
-    num == str_num.as_bytes().iter().map(|c| (((c - b'0') as u32).pow(len) as u32)).sum()
+    num == num_str.chars().map(|c| c.to_digit(10).unwrap().pow(len)).sum()
 }
