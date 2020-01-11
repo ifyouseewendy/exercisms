55a6ff54011609a8a386b51db1b4ecfbe708c759
Di Wen <ifyouseewendy@gmail.com>
2020-01-11 10:09:37 -0500

    rs: Refactor proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -3,14 +3,12 @@ pub fn build_proverb(list: &[&str]) -> String {
         return "".to_string();
     }
 
-    (0..list.len())
-        .map(|i| {
-            if i == list.len() - 1 {
-                format!("And all for the want of a {}.", list[0])
-            } else {
-                format!("For want of a {} the {} was lost.", list[i], list[i + 1])
-            }
-        })
-        .collect::<Vec<String>>()
+    list.windows(2)
+        .map(|w| format!("For want of a {} the {} was lost.", w[0], w[1]))
+        .chain(std::iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect::<Vec<_>>()
         .join("\n")
 }
