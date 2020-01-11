c26a10960dfd10ac13d9bd8b3c329eccce5bbac7
Di Wen <ifyouseewendy@gmail.com>
2020-01-10 22:18:54 -0500

    rs: Refactor padlindrome

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -1,6 +1,5 @@
 #[derive(Debug, PartialEq, Eq)]
 pub struct Palindrome {
-    // implement your palindrome type here
     factors: Vec<(u64, u64)>,
     value: u64,
 }
@@ -8,8 +7,8 @@ pub struct Palindrome {
 impl Palindrome {
     pub fn new(a: u64, b: u64) -> Palindrome {
         Palindrome {
-            factors: vec!((a, b)),
-            value: a*b,
+            factors: vec![(a, b)],
+            value: a * b,
         }
     }
 
@@ -20,54 +19,39 @@ impl Palindrome {
     pub fn insert(&mut self, a: u64, b: u64) {
         self.factors.push((a, b));
     }
-}
 
-fn find_factors(num: u64, min: u64, max: u64) -> Palindrome {
-    let mut factors = vec!();
-
-    for i in min..=max {
-        if num % i == 0 && num/i <= max {
-            factors.push((i, num/i))
+    pub fn from(factors: Vec<(u64, u64)>) -> Palindrome {
+        let (a, b) = factors[0];
+        Palindrome {
+            factors,
+            value: a * b,
         }
-        if i*i >= num { break }
     }
-
-    println!("factors: {:?}", factors);
-    if factors.is_empty() { panic!("factors is empty for {}, {}, {}", num, min, max) }
-
-    let (a, b) = factors[0];
-    let mut p = Palindrome::new(a, b);
-
-    let mut i = 1;
-    while i < factors.len() {
-        let (a, b) = factors[i];
-        p.insert(a, b);
-
-        i += 1;
-    }
-
-    p
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    if min > max { return None; }
+    if min > max {
+        return None;
+    }
 
     let candidates = find_candidates(min, max);
 
     let (min_p, max_p) = find_palindrome(&candidates)?;
-
     println!("min_p: {}, max_p: {}", min_p, max_p);
 
-    Some((find_factors(min_p, min, max), find_factors(max_p, min, max)))
+    Some((
+        Palindrome::from(find_factors(min_p, min, max)),
+        Palindrome::from(find_factors(max_p, min, max)),
+    ))
 }
 
 fn find_candidates(min: u64, max: u64) -> Vec<u64> {
-    let mut candidates = vec!();
+    let mut candidates = vec![];
 
     let numbers: Vec<u64> = (min..=max).collect();
     for i in 0..numbers.len() {
         for j in i..numbers.len() {
-            candidates.push(numbers[i]*numbers[j]);
+            candidates.push(numbers[i] * numbers[j]);
         }
     }
 
@@ -81,7 +65,9 @@ fn find_palindrome(candidates: &[u64]) -> Option<(u64, u64)> {
     let min = candidates.iter().find(|&&x| is_palindrome(x));
     let max = candidates.iter().rev().find(|&&x| is_palindrome(x));
 
-    if min.is_none() || max.is_none() { return None }
+    if min.is_none() || max.is_none() {
+        return None;
+    }
 
     Some((*min.unwrap(), *max.unwrap()))
 }
@@ -93,11 +79,33 @@ fn is_palindrome(num: u64) -> bool {
     let mut j = chars.len() - 1;
 
     while i <= j {
-        if chars[i] != chars[j] { return false; }
+        if chars[i] != chars[j] {
+            return false;
+        }
 
         i += 1;
-        if j > 1 { j -= 1; }
+        if j > 1 {
+            j -= 1;
+        }
     }
 
     true
+}
+
+fn find_factors(num: u64, min: u64, max: u64) -> Vec<(u64, u64)> {
+    let mut factors = vec![];
+
+    for i in min..=max {
+        if num % i == 0 && num / i <= max {
+            factors.push((i, num / i))
+        }
+        if i * i >= num {
+            break;
+        }
+    }
+    if factors.is_empty() {
+        panic!("factors is empty for {}, {}, {}", num, min, max)
+    }
+
+    factors
 }
